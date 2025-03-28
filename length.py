d7442f98dd9c2c8cecf7254bcfd4bb031a4e4e32
Rohit Sujith <102662018+rotti10@users.noreply.github.com>
2025-03-28 17:54:03 +0530

    Band name

---
+++ length.py
@@ -0,0 +1,4 @@
+print("welcome to the band name generator")  
+city_name = input("Which city did you grow up in ?\n ")
+pet_name = input("What is your pets name?\n")
+print("Your band name could be " + city_name + " " + pet_name)
